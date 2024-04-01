365fe638c6643957ce682a5c2e638db24b1cf7c5
baeyunhyeok <yunhyeok0928@gmail.com>
2024-04-01 17:23:32 +0900

    Establishment of development environment

---
+++ Devs_H/src/main/java/Devs_H/Devs_H/DevsHApplication.java
@@ -0,0 +1,13 @@
+package Devs_H.Devs_H;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsHApplication.class, args);
+	}
+
+}
---
+++ Devs_H/src/test/java/Devs_H/Devs_H/DevsHApplicationTests.java
@@ -0,0 +1,13 @@
+package Devs_H.Devs_H;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsHApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
